953423b393c5ca4b5d008ff102c18bdd87bc53c9
Katerina Navab <94501766+katerinanavab@users.noreply.github.com>
2024-02-05 13:25:07 -0500

    Update Constants.java
    Comparing our code to REV Ion 2024 code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,19 +13,28 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 
 public final class Constants {
   public static class SwerveConstants {
+    
     public static final MotorType kBrushless = MotorType.kBrushless;
 
-    public static final double kP = 1; //needs tuning
-    public static final double kI = 0.1;
-    public static final double kD = 1;
-
+    // MK4i Module Constants
     public static final double kDriveGearRatio = 6.75;
     public static final double kTurnGearRatio = 150/7;
-    public static final int kWheelDiameter = 4;
-    
-    public static final double kMaxMeterPerSec = feetToMeters(5) ; // 15.1 L2 drivetrain free speed
-    public static final double kRadToMotor = 1.570796;
+    public static final int kWheelDiameter = 4; // inches
 
+    // TODO: Check this section against REV code
+    public static final double kFreeSpeedRpm = 5676; // NEO motor free speed
+    public static final double kMaxMeterPerSec = feetToMeters(5) ; // 15.1 L2 drivetrain free speed
+    public static final double kRadToMotor = 1.570796; // what is this?
+    public static final double kSlewRate = 1.8; // updated to match REV
+    public static final double kTurnSlewRate = 2.0; // updated to match REV
+    public static final double kDeadZone = 0.05; // updated to match REV (kDriveDeadband)
+
+    // PID Constants (NOTE: REV code has different #s for driving & turning)
+    public static final double kP = 0.04; // updated to match REV
+    public static final double kI = 0; // updated to match REV
+    public static final double kD = 0; // updated to match REV
+
+    // SparkMax CAN IDs
     public static final int kFlDrive = 2;
     public static final int kFlTurn = 1;
     public static final int kFrDrive = 4;
@@ -34,28 +43,28 @@ public final class Constants {
     public static final int kBrTurn = 5;
     public static final int kBlDrive = 8;
     public static final int kBlTurn = 7;
-
     public static final int kFlEncoder = 0;
     public static final int kFrEncoder = 1;
     public static final int kBlEncoder = 3;
     public static final int kBrEncoder = 2;
 
-    public static final double kFlOffset = 0; //all need to be tested (rad)
+    // Angular Offsets (values updated to match REV)
+    public static final double kFlOffset = -Math.PI / 2;
     public static final double kFrOffset = 0;
-    public static final double kBlOffset = 0;
-    public static final double kBrOffset = 0;
+    public static final double kBlOffset = Math.PI;
+    public static final double kBrOffset = Math.PI / 2;
 
-    public static final double kChassisWidth = inchesToMeters(30.25); //idk either of these
-    public static final double kChassisLength = inchesToMeters(30.25);
-
+    // Chassis Configuration
+    // Distance btwn centers of R & L wheels (kTrackWidth in REV)
+    public static final double kChassisWidth = inchesToMeters(21.5); // updated to match REV
+    // Distance between F & B wheels (kWheelBase in REV)
+    public static final double kChassisLength = inchesToMeters(21.5); // updated to match REV
     public static final SwerveDriveKinematics kKinematics = new SwerveDriveKinematics(
       new Translation2d(kChassisLength/2, -kChassisWidth/2),
       new Translation2d(kChassisLength/2, kChassisWidth/2),
       new Translation2d(-kChassisLength/2, -kChassisWidth/2),
       new Translation2d(-kChassisLength/2, kChassisWidth/2)
     );
-    public static final double kSlewRate = 1;
-    public static final double kTurnSlewRate = 2;
-    public static final double kDeadZone = 0.1;
+    
   }
 }
